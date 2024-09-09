8e84350796e2b1371b0f090ab01767e0f076125f
lautaroperesin <lautaroperesin@gmail.com>
2024-09-09 09:02:14 -0300

    Agregando lista de equipos en la página de Ranking

--- src/app/ranking/page.jsx
+++ src/app/ranking/page.jsx
@@ -5,6 +5,8 @@ export const metadata = {
 }
 
 export default function Ranking() {
+    const equipos = ["River", "Boca", "Racing", "Independiente", "San Lorenzo", "Huracan", "Velez", "Lanus", "Banfield", "Estudiantes", "Gimnasia", "Platense", "Argentinos", "Tigre", "Chacarita"];
+
     return(
         <>
         <h1>Ranking del Prode - Fecha 16</h1>
@@ -14,6 +16,12 @@ export default function Ranking() {
             <li>3. Usuario 3 - 17 pts</li>
             <li>4. Usuario 4 - 16 pts</li>
         </ul>
+        <h2>Equipos</h2>
+        <ol>
+          {
+          equipos.map((equipo,index)=> <li key={index}>{equipo}</li>)
+          }
+        </ol>
         </>
     );
 }
